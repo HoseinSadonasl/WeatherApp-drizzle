82c1a33aa55dd9fcf9a1b20ea2cc70dc4097e708
Dev Hosein <413721993>
2022-07-04 02:10:16 +0430

    migrate to flow

--- app/src/main/java/com/hoseinsadonasl/weatherapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/hoseinsadonasl/weatherapp/ui/viewmodels/MainViewModel.kt
@@ -6,9 +6,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hoseinsadonasl.weatherapp.models.Current
+import com.hoseinsadonasl.weatherapp.models.Daily
 import com.hoseinsadonasl.weatherapp.models.Weather
 import com.hoseinsadonasl.weatherapp.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,15 +31,72 @@ class MainViewModel @Inject constructor(
     val currentWeather: LiveData<Current>
         get() = _currentWeather
 
+    private var _dailytWeather = MutableLiveData<List<Daily>>()
+    val dailytWeather: LiveData<List<Daily>>
+        get() = _dailytWeather
+
+    private var _timeZone = MutableLiveData<Double>()
+    val timeZone: LiveData<Double>
+        get() = _timeZone
+
+    private var _temp = MutableLiveData<Double>()
+    val temp: LiveData<Double>
+        get() = _temp
+
+    private var _feels_like = MutableLiveData<Double>()
+    val feels_like: LiveData<Double>
+        get() = _feels_like
+
+    private var _sunrise = MutableLiveData<Double>()
+    val sunrise: LiveData<Double>
+        get() = _sunrise
+
+    private var _sunset = MutableLiveData<Double>()
+    val sunset: LiveData<Double>
+        get() = _sunset
+
+    private var _humidity = MutableLiveData<Double>()
+    val humidity: LiveData<Double>
+        get() = _humidity
+
+    private var _dewPoint = MutableLiveData<Double>()
+    val dewPoint: LiveData<Double>
+        get() = _dewPoint
+
+    private var _uvi = MutableLiveData<Double>()
+    val uvi: LiveData<Double>
+        get() = _uvi
+
+    private var _description = MutableLiveData<String>()
+    val description: LiveData<String>
+        get() = _description
+
+    private var _icon = MutableLiveData<String>()
+    val icon: LiveData<String>
+        get() = _icon
+
     init {
         getCurrentWeather()
+        getDailyWeather()
     }
 
     private fun getCurrentWeather() {
-        viewModelScope.launch {
-            _currentWeather.value = repository.getWeather().body()?.current
-            Log.d(TAG, "getCurrentWeather: ${repository.getWeather().body()}")
+        CoroutineScope(IO).launch {
+            delay(5000)
+            repository.getWeather().collect {
+                _currentWeather.postValue(it.body()?.current)
+                Log.d(TAG, "getDailyWeather: ${it.body()?.current}")
+            }
+        }
+    }
 
+    private fun getDailyWeather() {
+        CoroutineScope(IO).launch {
+            delay(5000)
+            repository.getWeather().collect {
+                _dailytWeather.postValue(it.body()?.daily)
+                Log.d(TAG, "getDailyWeather: ${it.body()?.daily?.size}")
+            }
         }
     }
 }
